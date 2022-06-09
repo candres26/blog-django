54376356b0c1a722aef0ae78aadc9641919ec9a6
Carlos Andres Reyes Andrade <karlosareyes@gmail.com>
2022-06-09 10:04:18 -0500

    Creacion de las migraciones, formularios del post, listado de post y vista de detalle del post

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,11 @@
 from re import A
 from django.urls import path
+from .views import BlogCreateView, BlogListView, BlogDetailView
 
 app_name="blog"
 
 urlpatterns = [
-    
+    path('', BlogListView.as_view(), name="home"),
+    path('create/', BlogCreateView.as_view(), name="create"),
+    path('<int:pk>/', BlogDetailView.as_view(), name="detail")
 ]
