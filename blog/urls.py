4d09480c3b2946f32f687f2889fe7fc6052be76c
Carlos Andres Reyes Andrade <karlosareyes@gmail.com>
2022-06-13 10:30:21 -0500

    Funcion de actualizacion de post agregada

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,12 @@
 from re import A
 from django.urls import path
-from .views import BlogCreateView, BlogListView, BlogDetailView
+from .views import BlogCreateView, BlogListView, BlogDetailView, BlogUpdateView
 
 app_name="blog"
 
 urlpatterns = [
     path('', BlogListView.as_view(), name="home"),
     path('create/', BlogCreateView.as_view(), name="create"),
-    path('<int:pk>/', BlogDetailView.as_view(), name="detail")
+    path('<int:pk>/', BlogDetailView.as_view(), name="detail"),
+    path('<int:pk>/update/', BlogUpdateView.as_view(), name="update")
 ]
